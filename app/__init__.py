4d67121a8f1e27b4658d533d7a2570c3fdc4378c
endoffile78 <endoffile78@yahoo.com>
2020-02-22 13:48:56 -0600

    Change datase file name

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from app.db import dbinit
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
+
     dbinit(app)
+
     from app.receiver import bp as reciever_bp
     app.register_blueprint(reciever_bp)
 
