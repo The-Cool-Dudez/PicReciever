323c2b1e98c48783cab93ad064aaf38cec0e5e3c
endoffile78 <endoffile78@yahoo.com>
2020-02-22 13:40:04 -0600

    Fix model and add constructor

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from app.db import db
 
 class Images(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    id = db.Column(db.String(5000), unique=True, nullable=False)
+    id = db.Column(db.Integer(), primary_key=True)
+    path = db.Column(db.String(5000), unique=True, nullable=False)
+
+    def __init__(self, path):
+        self.path = path
